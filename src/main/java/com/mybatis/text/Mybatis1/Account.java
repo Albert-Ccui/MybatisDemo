95047c588499af98d4bc3ee7e3d271b008582ccd
Albert-Ccui <2569372950@qq.com>
2017-09-19 14:44:55 +0800

    Mybatis01

---
+++ src/main/java/com/mybatis/text/Mybatis1/Account.java
@@ -0,0 +1,26 @@
+package com.mybatis.text.Mybatis1;
+public class Account {
+	private int id;
+	private String name;
+	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
